282b5fae80641653b71b297f6bd3caa485dc8c9d
Stevenner <stevenchris232@gmail.com>
2024-04-05 18:12:04 +0700

    new file:   database/migrations/2024_04_05_110626_create_products_table.php

---
+++ database/migrations/2024_04_05_110626_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        DB::statement("insert into products (id, name, price) 	values (1, 'ASUS', 15000000)");
+        DB::statement("insert into products (id, name, price) 	values (2, 'Lenovo', 13500000)");
+        DB::statement("insert into products (id, name, price) 	values (3, 'HP', 17000000)");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
